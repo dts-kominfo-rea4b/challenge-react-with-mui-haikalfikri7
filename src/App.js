5fcdc43839ad0a14cc1f48546813ce60bcbcca77
Haikal Fikri <fhaikal580@gmail.com>
2022-09-27 21:01:23 +0700

    tugas challenge App.js

--- src/App.js
+++ src/App.js
@@ -1,9 +1,9 @@
-import './App.css';
-import Contact from "./components/Contact";
-import Header from "./components/Header";
-import ContactForm from "./components/ContactForm";
-import Grid from "@mui/material/Grid";
+import { Card, Grid, List } from "@mui/material";
 import React, { useState } from "react";
+import "./App.css";
+import Contact from "./components/Contact";
+import ContactForm from "./components/ContactForm";
+import Header from "./components/Header";
 // Uncomment untuk memuat daftar kontak
 import contactsJSON from "./data/contacts.json";
 // import contactsJSON from './data/contacts.json';
@@ -13,25 +13,42 @@ const App = () => {
   // https://mui.com/material-ui/react-list/#folder-list
 
   // Masukkan contacts yang sudah didapat dalam JSON sebagai initial state
+  const [listContact, setcontact] = useState(contactsJSON);
   // Buatlah handler untuk menambahkan kontak baru yang akan dikirim ke ContactForm
-  const [contacts, setContacts] = useState(contactJSON);
+  
 
-  const handleSubmit = (event) => {
-    console.log("event ", event);
-    setContacts([...contacts, event]);
+  const addContact = (data) => {
+    const newContact = [...listContact, data];
+    setcontact(newContact);
   };
 
   return (
     <div className="App">
-       <Header />
-      <Grid container spacing={1}>
-        <Grid item xs={7}>
-          <ContactForm handleClick={handleSubmit} />
+       <Grid
+        container
+        spacing={2}
+        direction="row"
+        justifyContent="center"
+        alignItems="center"
+      >
+        <Grid item xs={12}>
+          <Header />
         </Grid>
-        <Grid item xs={5} sx={{ marginTop: 8 }}>
-          {contacts.map((contact, index) => (
-            <Contact key={index} data={contact} />
-          ))}
+        <Grid item xs={4}>
+          <Card variant="outlined">
+            <ContactForm addContact={addContact} />
+          </Card>
+        </Grid>
+        <Grid item xs={4}>
+          <Card variant="outlined">
+            {listContact.map((item, index) => {
+              return (
+                <List sx={{ width: "100%", maxWidth: 360 }} key={index}>
+                  <Contact data={item} />
+                </List>
+              );
+            })}
+          </Card>
         </Grid>
       </Grid>
     </div>
