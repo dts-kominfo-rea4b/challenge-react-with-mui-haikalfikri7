50242a180641021ba1519ca519234cd46e4f230b
Haikal Fikri <fhaikal580@gmail.com>
2022-09-27 21:05:39 +0700

    tugas react Contact.js

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,8 +1,15 @@
 // Terima props lalu tampilkan dalam Contact component
 // Contact component dapat berupa MUI ListItem
 // https://mui.com/material-ui/react-list/#folder-list
-import {Avatar, Card, CardContent, Divider, List, ListItem, ListItemAvatar, ListItemText, Typhography } from "@mui/material"
-import React from 'react';
+import {
+    Avatar,
+    ListItem,
+    ListItemAvatar,
+    ListItemText,
+    Divider,
+    Typography,
+  } from "@mui/material";
+  import React from "react";
 
 // Kalian bisa membuat CSS sendiri di src/components/Contact.css
 // atau langsung tambahkan dengan sx={{}}
@@ -10,27 +17,32 @@ const Contact = ({ data }) => {
     // Contact berisi foto, nama, telepon, dan email
     return (
         <>
-          <ListItem>
-            <ListItemAvatar>
-              <Avatar src={data.photo} />
-            </ListItemAvatar>
-            <ListItemText
-              primary={data.name}
-              secondary={
-                <React.Fragment>
-                  <Typography component="span" variant="body2" color="text.primary">
-                    {data.phone}
-                  </Typography>
-                  <br />
-                  {data.email}
-                </React.Fragment>
-              }
-            />
-          </ListItem>
-          <Divider variant="inset" component="li" />
+            <ListItem alignItems="flex-start">
+        <ListItemAvatar>
+          <Avatar alt={data.name} src={data.photo}></Avatar>
+        </ListItemAvatar>
+        <ListItemText
+          primary={data.name}
+          secondary={
+            <React.Fragment>
+              <Typography
+                sx={{ display: "inline" }}
+                component="span"
+                variant="body2"
+                color="text.primary"
+              >
+                {data.phone}
+              </Typography>
+              <br />
+              {data.email}
+            </React.Fragment>
+          }
+        ></ListItemText>
+      </ListItem>
+      <Divider variant="inset" component="li" />
         </>
-      );
-    };
+    );
+};
 
    
 
